8b0471f351b136e36293e58b61005e86b273f4ae
saravanakumar <saravanakumar@localhost>
2019-07-07 22:31:21 +0530

    Get user by name API added

--- src/main/java/room/management/controller/UserController.java
+++ src/main/java/room/management/controller/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,21 +20,20 @@ import room.management.repository.UsersRepository;
 public class UserController {
 
 	@Autowired
-	UserController itemService;
-
-	@Autowired
 	private UsersRepository usersRepository;
 
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping("")
 	@ResponseBody
-	public ResponseEntity<List<User>> getAllItems() {
+	public ResponseEntity<List<User>> getAllUsers() {
 		return new ResponseEntity<List<User>>(usersRepository.findAll(), HttpStatus.OK);
 	}
 
-	@RequestMapping("/hello")
-	public String sayHello() {
-		return "Hello User!";
+	@GetMapping(value = "/{name}", headers = "Accept=application/json")
+	public ResponseEntity<User> getUserByName(@RequestParam  (value = "name") String name) {
+		User dbVal = usersRepository.findByFirstName(name).get();
+		return new ResponseEntity<User>(dbVal, HttpStatus.OK);
 	}
+
 
 }
